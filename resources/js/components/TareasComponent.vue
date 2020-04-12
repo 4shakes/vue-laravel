6b2a156da188195fe8817f58345f4bce878309cc
jayro <jayro1996.ag@gmail.com>
2020-04-12 17:28:29 -0500

    Se realizo el CRUD en laravel y el formulario
    Se realizo el crud en el backend y la vista del formulario en el componente
    TareasComponent

---
+++ resources/js/components/TareasComponent.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h3>Agregar Tareas</h3>
+        <form @submit.prevent="agregar">
+            <input
+                type="text"
+                placeholder="Nombre"
+                class="form-control mb-2"
+                v-model="tarea.nombre"
+            />
+            <input
+                type="text"
+                placeholder="Descripcion"
+                class="form-control mb-2"
+                v-model="tarea.descripcion"
+            />
+            <button class="btn btn-block btn-success" type="submit">
+                Enviar
+            </button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            tareas: [],
+            tarea: {
+                nombre: "",
+                descripcion: ""
+            }
+        };
+    },
+    methods: {
+        agregar() {
+            console.log(this.tarea.nombre, this.tarea.descripcion);
+        }
+    }
+};
+</script>
+
+<style></style>
